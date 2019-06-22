c580cf374f26c84c8bb9abca63cd7f511c45cf0c
Luís Jaeger <904975@lojasrenner.com.br>
2019-06-22 10:12:37 -0300

    feat(login-structure)

---
+++ ipet/Login/LoginInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  LoginInteractor.swift
+//  ipet
+//
+//  Created by Luís Ricardo Jaeger on 22/06/19.
+//  Copyright © 2019 Luís Ricardo Jaeger. All rights reserved.
+//
+
+import Foundation
---
+++ ipet/Login/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  ipet
+//
+//  Created by Luís Ricardo Jaeger on 22/06/19.
+//  Copyright © 2019 Luís Ricardo Jaeger. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
